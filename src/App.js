9deb7eb7ddc0765daa53580d0c633bea9810fe4d
Joshua Lee <jleey2k@gmail.com>
2020-01-05 20:51:12 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,6 +30,27 @@ class App extends React.Component {
       <div id="body">
         <div id="class header"> </div>
         <div id="list of people"> </div>
+
+        <script src="https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/7.6.1/firebase-analytics.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/7.6.1/firebase-firestore.js"></script>
+
+        <script>
+          // Your web app's Firebase configuration
+          var firebaseConfig = {
+            apiKey: "AIzaSyDVdohGwcKZibRusfG6IGCq3CBFCVGdka0",
+            authDomain: "schedule-comparator.firebaseapp.com",
+            databaseURL: "https://schedule-comparator.firebaseio.com",
+            projectId: "schedule-comparator",
+            storageBucket: "schedule-comparator.appspot.com",
+            messagingSenderId: "521939045676",
+            appId: "1:521939045676:web:6cdf2357f23e33e58fb089",
+            measurementId: "G-V4VQ51GDDZ"
+          };
+          // Initialize Firebase
+          firebase.initializeApp(firebaseConfig);
+          firebase.analytics();
+        </script>
       </div>
       </div>
     );
