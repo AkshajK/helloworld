9d3b2478e98630016056f08a5526cadd956d770c
dansun123 <41485135+dansun123@users.noreply.github.com>
2020-01-06 14:13:09 -0500

    daniel sun added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+//Daniel Sun is here
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import * as firebase from "firebase/app";
 
