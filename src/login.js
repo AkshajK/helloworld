5ed1d55a31f6373869f974f70bcc93ca82c6634e
dansun123 <41485135+dansun123@users.noreply.github.com>
2020-01-07 15:43:44 -0500

    login works

--- src/login.js
+++ src/login.js
@@ -2,15 +2,62 @@ import React, { useState } from "react";
 import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
 import "./login.css";
 
+import * as firebase from "firebase/app";
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDVdohGwcKZibRusfG6IGCq3CBFCVGdka0",
+    authDomain: "schedule-comparator.firebaseapp.com",
+    databaseURL: "https://schedule-comparator.firebaseio.com",
+    projectId: "schedule-comparator",
+    storageBucket: "schedule-comparator.appspot.com",
+    messagingSenderId: "521939045676",
+    appId: "1:521939045676:web:6cdf2357f23e33e58fb089",
+    measurementId: "G-V4VQ51GDDZ"
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+
 export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   function validateForm() {
-    return email.length > 8 && password.length > 0 && email.indexOf("@mit.edu")>=0;
+    return email.length > 8  && password.length > 0;
   }
 
   function handleSubmit(event) {
+    let kerb = email.substring(0, email.indexOf('@'))
+    var docRef = db.collection("users").doc(kerb);
+
+    docRef.get().then(function(doc) {
+        if (doc.exists) {
+            if (doc.data()['password'] == password){
+              alert("login successful. Welcome")
+            } else {
+              console.log(password)
+              console.log(doc.data()['password'])
+              alert("fuck you poser")
+            }
+            console.log("Document data:", doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
     event.preventDefault();
   }
 
