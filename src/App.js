a7f3af35354442c78e70ac3bcf4bc46e639dcf02
Joshua Lee <jleey2k@gmail.com>
2020-01-11 16:51:29 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -475,13 +475,13 @@ class App extends React.Component {
               <div className = 'nav1'><Link to="/register">Register</Link></div>
 
 
-              {/*
-                A <Switch> looks through all its children <Route>
-                elements and renders the first one whose path
-                matches the current URL. Use a <Switch> any time
-                you have multiple routes, but you want only one
-                of them to render at a time
-              */}
+              
+              //  A <Switch> looks through all its children <Route>
+              //  elements and renders the first one whose path
+              //  matches the current URL. Use a <Switch> any time
+              //  you have multiple routes, but you want only one
+              //  of them to render at a time
+              
               <Switch>
                 <Route exact path="/">
                 </Route>
